4ccac3124dd9ada1aaca5d56d0915dfce470d126
unknown <erickmercadooudalova@gmail.com>
2025-08-21 12:57:32 -0400

    desarrollo borrado por jhenyfer

--- pdv_genera_cuentas_analiticas/models/pos_config.py
+++ pdv_genera_cuentas_analiticas/models/pos_config.py
@@ -1,5 +1,10 @@
+from odoo import api, models, fields
+
+import logging
+_logger = logging.getLogger(__name__)
+
 class PosConfig(models.Model):
-    _name = 'pos.config'
+    _inherit = 'pos.config'
     
     pos_analytic_account_ids = fields.Many2many(
         'account.analytic.account', 
