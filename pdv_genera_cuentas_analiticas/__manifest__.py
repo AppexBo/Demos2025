f9692dee95906fe643ce8e35df087393c894926c
AppexBo <108677502+AppexBo@users.noreply.github.com>
2025-08-21 12:22:52 -0400

    Revert "quitar desarrollos"
    This reverts commit 788f9cb73351c728e70d4301dcf3a61516154dab.

---
+++ pdv_genera_cuentas_analiticas/__manifest__.py
@@ -0,0 +1,22 @@
+{
+	"name" : "PDV genera cuenta analiticas",
+	"version" : "17.0.0.0",
+	"category" : "Point of Sale",
+	"depends" : [
+		'base',
+		'point_of_sale',
+		'account',
+		'analytic',
+	],
+	"author": "AppexBo",
+	'summary': 'Cuando se genere una venta o devolucion de algun producto en el punto de venta este se asignara y generara su cuenta analitica correspondiente de entrada o salida segun la sucursal',
+	"description": "Cuando se genere una venta o devolucion de algun producto en el punto de venta este se asignara y generara su cuenta analitica correspondiente de entrada o salida segun la sucursal",
+	"website" : "https://www.appexbo.com/",
+	"auto_install": False,
+	"installable": True,
+	'application': True,
+	"license": "LGPL-3",
+	"data": [
+        #'views/pos_order_line_form.xml'
+	],
+}
