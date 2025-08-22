352bd872d7b3e779f222505679a46367f5f75993
unknown <erickmercadooudalova@gmail.com>
2025-08-22 10:14:23 -0400

    prueba

--- pdv_genera_cuentas_analiticas/models/account_move.py
+++ pdv_genera_cuentas_analiticas/models/account_move.py
@@ -16,23 +16,32 @@ class AccountMove(models.Model):
 
     def _asignar_distribucion_analitica(self, move):
         try:
-            # Buscar la primera cuenta analítica disponible
-            analytic_account = self.env['account.analytic.account'].search([], limit=1)
-            
-            if analytic_account:
-                # Preparar la distribución analítica (100% para la cuenta encontrada)
-                analytic_distribution = {str(analytic_account.id): 100.0}
+            analytic_account_ids = self._get_analytic_account_ids(move)
+            if analytic_account_ids:
+                # Buscar la primera cuenta analítica disponible
+                analytic_account = self.env['account.analytic.account'].search([], limit=1)
                 
-                # Recorrer todas las líneas del movimiento y asignar la distribución analítica
-                for line in move.line_ids:
-                    line.write({
-                        'analytic_distribution': analytic_distribution
-                    })
-                
-                _logger.info("Distribución analítica asignada al move %s: %s", move.name, analytic_distribution)
-            else:
-                _logger.warning("No se encontraron cuentas analíticas para asignar al move %s", move.name)
-                
+                if analytic_account:
+                    # Preparar la distribución analítica (100% para la cuenta encontrada)
+                    analytic_distribution = {str(analytic_account.id): 100.0}
+                    
+                    # Recorrer todas las líneas del movimiento y asignar la distribución analítica
+                    for line in move.line_ids:
+                        line.write({
+                            'analytic_distribution': analytic_distribution
+                        })
+                    #_logger.info("Distribución analítica asignada al move %s: %s", move.name, analytic_distribution)
+                else:
+                    _logger.warning("No se encontraron cuentas analíticas para asignar al move %s", move.name)
         except Exception as e:
             _logger.error("Error al asignar distribución analítica al move %s: %s", move.name, str(e))
-        
+    
+    def _get_analytic_account_ids(self, move):
+        analytic_account_ids = None
+        # Buscar el pos.order donde el name sea igual al move_ref
+        pos_order = self.env['pos.order'].search([('name', '=', move.ref)], limit=1)
+        if pos_order:
+            pos_session = pos_order.session_id
+                if pos_session: 
+                    analytic_account_ids = pos_config.analytic_account_ids
+        return analytic_account_ids
