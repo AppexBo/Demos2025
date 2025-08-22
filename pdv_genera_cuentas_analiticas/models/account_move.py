a51ec61c2b3da3b2a3a7c3d4c75b1b333073d66e
unknown <erickmercadooudalova@gmail.com>
2025-08-22 09:33:32 -0400

    prueba

--- pdv_genera_cuentas_analiticas/models/account_move.py
+++ pdv_genera_cuentas_analiticas/models/account_move.py
@@ -9,6 +9,30 @@ class AccountMove(models.Model):
     @api.model
     def create(self, vals):
         vals_copy = vals.copy()
-        _logger.info("CREATING AccountMove with values: %s", vals_copy)
-        return super(AccountMove, self).create(vals)
+        move = super(AccountMove, self).create(vals)
+        if vals.get('invoice_origin') and vals.get('move_type') == 'out_invoice':
+            self._asignar_distribucion_analitica(move)
+        return move
+
+    def _asignar_distribucion_analitica(self, move):
+        try:
+            # Buscar la primera cuenta analítica disponible
+            analytic_account = self.env['account.analytic.account'].search([], limit=1)
+            
+            if analytic_account:
+                # Preparar la distribución analítica (100% para la cuenta encontrada)
+                analytic_distribution = {str(analytic_account.id): 100.0}
+                
+                # Recorrer todas las líneas del movimiento y asignar la distribución analítica
+                for line in move.line_ids:
+                    line.write({
+                        'analytic_distribution': analytic_distribution
+                    })
+                
+                _logger.info("Distribución analítica asignada al move %s: %s", move.name, analytic_distribution)
+            else:
+                _logger.warning("No se encontraron cuentas analíticas para asignar al move %s", move.name)
+                
+        except Exception as e:
+            _logger.error("Error al asignar distribución analítica al move %s: %s", move.name, str(e))
         
